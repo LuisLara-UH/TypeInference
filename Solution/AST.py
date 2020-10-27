9bbadf3b54a789953f064151c4ea2b6001aee4ba
Luis Lara <luis.lara.uh42@gmail.com>
2020-10-26 23:45:28 -0400

    Update AST

--- Solution/AST.py
+++ Solution/AST.py
@@ -2,29 +2,49 @@ class Node:
     pass
 
 class ProgramNode(Node):
-    def __init__(self, statements):
-        self.statements = statements
-        
-class StatementNode(Node):
+    def __init__(self, declarations):
+        self.declarations = declarations
+
+class DeclarationNode(Node):
     pass
-        
+
 class ExpressionNode(Node):
     pass
 
-class VarDeclarationNode(StatementNode):
+class ClassDeclarationNode(DeclarationNode):
+    def __init__(self, idx, features, parent=None):
+        self.id = idx
+        self.parent = parent
+        self.features = features
+
+class FuncDeclarationNode(DeclarationNode):
+    def __init__(self, idx, params, return_type, body):
+        self.id = idx
+        self.params = params
+        self.type = return_type
+        self.body = body
+
+class AttrDeclarationNode(DeclarationNode):
+    def __init__(self, idx, typex):
+        self.id = idx
+        self.type = typex
+
+class VarDeclarationNode(ExpressionNode):
+    def __init__(self, idx, typex, expr):
+        self.id = idx
+        self.type = typex
+        self.expr = expr
+
+class AssignNode(ExpressionNode):
     def __init__(self, idx, expr):
         self.id = idx
         self.expr = expr
 
-class FuncDeclarationNode(StatementNode):
-    def __init__(self, idx, params, body):
+class CallNode(ExpressionNode):
+    def __init__(self, obj, idx, args):
+        self.obj = obj
         self.id = idx
-        self.params = params
-        self.body = body
-
-class PrintNode(StatementNode):
-    def __init__(self, expr):
-        self.expr = expr
+        self.args = args
 
 class AtomicNode(ExpressionNode):
     def __init__(self, lex):
@@ -37,23 +57,15 @@ class BinaryNode(ExpressionNode):
 
 class ConstantNumNode(AtomicNode):
     pass
-
 class VariableNode(AtomicNode):
     pass
-
-class CallNode(AtomicNode):
-    def __init__(self, idx, args):
-        AtomicNode.__init__(self, idx)
-        self.args = args
-
+class InstantiateNode(AtomicNode):
+    pass
 class PlusNode(BinaryNode):
     pass
-
 class MinusNode(BinaryNode):
     pass
-
 class StarNode(BinaryNode):
     pass
-
 class DivNode(BinaryNode):
     pass
